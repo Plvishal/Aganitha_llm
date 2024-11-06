d136562ae82f572195250299717051e23f43bd4d
Vishal <vishalpal1501@gmail.com>
2024-11-07 02:57:20 +0530

    padding

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,6 +4,7 @@ const SearchBar = ({ fetchRecipes }) => {
   const [ingredient, setIngredient] = useState('');
 
   const handleSearch = () => {
+    console.log(ingredient);
     if (ingredient.trim()) {
       fetchRecipes(ingredient);
     }
@@ -14,10 +15,12 @@ const SearchBar = ({ fetchRecipes }) => {
       <input
         type="text"
         placeholder="Enter an ingredient..."
+        name="search"
         value={ingredient}
         onChange={(e) => setIngredient(e.target.value)}
-        className="text-black rounded-lg shadow-lg p-28"
+        className="text-black rounded-lg shadow-lg p-6"
       />
+
       <button
         onClick={handleSearch}
         className="bg-blue-600 text-white px-5 rounded-md font-serif text-lg hover:bg-blue-700 transition-all shadow-lg"
